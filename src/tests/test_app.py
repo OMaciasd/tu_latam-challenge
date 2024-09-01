fe9ead02a39c3839e5582091f797bb4c7cbbe42b
Oscar Macias. <omaciasnarvaez@gmail.com>
2024-08-31 22:46:14 -0500

    Update test_app.py

--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -1,6 +1,6 @@
 import pytest
+from unittest.mock import patch
 from app.app import app
-
 
 @pytest.fixture
 def client():
@@ -8,9 +8,12 @@ def client():
     with app.test_client() as client:
         yield client
 
-
-def test_homepage(client):
-    """Test Web init."""
+@patch('app.app.create_rabbitmq_connection')
+def test_homepage(mock_create_rabbitmq_connection, client):
+    """Test Web init with RabbitMQ connection mocked."""
+    # Mockea la conexión a RabbitMQ
+    mock_create_rabbitmq_connection.return_value = (None, None)
+    
     response = client.get('/')
     assert response.status_code == 200
     assert b'Hello, World!' in response.data
